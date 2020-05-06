bd22b9e305ced7e611a8576fdd0fd29b615486aa
MamtaSonar <46754967+mamtasonar1127@users.noreply.github.com>
2020-05-06 11:39:01 -0400

    Connect to mySQL Workbench

--- model.php
+++ model.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn=new MySQLi("localhost","root","","tmsbrd");
+$conn=new MySQLi("us-cdbr-iron-east-01.cleardb.net","b794f66015c511","19936af4","heroku_abf85657748f742");
 
 
 class model
